35d2568a392eab489123cbb348d21890d4385e1c
Akira Chenyavanij <natersland@gmail.com>
2022-03-06 03:54:21 +0700

    no1: finished

---
+++ 001-Brand-Name-Generator/script.js
@@ -0,0 +1,4 @@
+console.log("Welcome to Band Name Generator");
+let birthCity = prompt("What'the name of the city you grew up in?");
+let petName = prompt("What'your pet name?");
+alert(`Your band name is "${birthCity} ${petName}"!`);
